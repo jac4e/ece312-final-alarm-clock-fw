54e9a92be5ca4515c3ea5a64ffb88ea736f8e30e
Jacques Fourie <jacques@fourie.ca>
2023-11-14 22:51:40 -0700

    Init MPLABX Project

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jacques
+ *
+ * Created on November 14, 2023, 10:44 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
