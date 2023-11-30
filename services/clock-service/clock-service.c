f57cc4bedee2fefc8d7d38c955c1911758445b3f
Jacques Fourie <jacques@fourie.ca>
2023-11-29 21:41:45 -0700

    Made starting time the time the code was compiled

--- services/clock-service/clock-service.c
+++ services/clock-service/clock-service.c
@@ -254,18 +254,109 @@ const uint16_t prescaler_values[] = {0, 1, 8, 32, 64, 128, 256, 1024};
 // Function to initialize the clock service
 void clock_service_init(clock_service *service)
 {
-    // Initialize service parameters
+    // Get build time from __DATE__ and __TIME__ macros
+    // These macros are defined by the compiler at compile time
+    // Format: "Mmm dd yyyy" and "hh:mm:ss"
+    // Example: "Feb 13 2021" and "12:00:00"
 
-    // Initialize time to epoch Midnight Jan 1 2000
-    service->_time.tm_sec = 0;
-    service->_time.tm_min = 0;
-    service->_time.tm_hour = 0;
-    service->_time.tm_mday = 0;
+    char build_date[12] = __DATE__;
+    char build_time[9] = __TIME__;
+
+
+    char month[4] = {0};
+    char day[3] = {0};
+    char year[5] = {0};
+    char hour[3] = {0};
+    char minute[3] = {0};
+    char second[3] = {0};
+
+    // Get month
+    month[0] = build_date[0];
+    month[1] = build_date[1];
+    month[2] = build_date[2];
+
+    // Get day
+    day[0] = build_date[4];
+    day[1] = build_date[5];
+
+    // Get year
+    year[0] = build_date[7];
+    year[1] = build_date[8];
+    year[2] = build_date[9];
+    year[3] = build_date[10];
+
+    // Get hour
+    hour[0] = build_time[0];
+    hour[1] = build_time[1];
+
+    // Get minute
+    minute[0] = build_time[3];
+    minute[1] = build_time[4];
+
+    // Get second
+    second[0] = build_time[6];
+    second[1] = build_time[7];
+
+    // Convert strings to integers and store in service->_time
+    service->_time.tm_sec = atoi(second);
+    service->_time.tm_min = atoi(minute);
+    service->_time.tm_hour = atoi(hour);
+    service->_time.tm_mday = atoi(day);
     service->_time.tm_wday = 0;
     service->_time.tm_mon = 0;
-    service->_time.tm_year = 100;
+    service->_time.tm_year = atoi(year) - 1900;
     service->_time.tm_yday = 0;
     service->_time.tm_isdst = 0;
+
+    // Convert month string to integer
+    if (strcmp(month, "Jan") == 0)
+    {
+        service->_time.tm_mon = 0;
+    }
+    else if (strcmp(month, "Feb") == 0)
+    {
+        service->_time.tm_mon = 1;
+    }
+    else if (strcmp(month, "Mar") == 0)
+    {
+        service->_time.tm_mon = 2;
+    }
+    else if (strcmp(month, "Apr") == 0)
+    {
+        service->_time.tm_mon = 3;
+    }
+    else if (strcmp(month, "May") == 0)
+    {
+        service->_time.tm_mon = 4;
+    }
+    else if (strcmp(month, "Jun") == 0)
+    {
+        service->_time.tm_mon = 5;
+    }
+    else if (strcmp(month, "Jul") == 0)
+    {
+        service->_time.tm_mon = 6;
+    }
+    else if (strcmp(month, "Aug") == 0)
+    {
+        service->_time.tm_mon = 7;
+    }
+    else if (strcmp(month, "Sep") == 0)
+    {
+        service->_time.tm_mon = 8;
+    }
+    else if (strcmp(month, "Oct") == 0)
+    {
+        service->_time.tm_mon = 9;
+    }
+    else if (strcmp(month, "Nov") == 0)
+    {
+        service->_time.tm_mon = 10;
+    }
+    else if (strcmp(month, "Dec") == 0)
+    {
+        service->_time.tm_mon = 11;
+    }
 
 // We can use the libc time functions if we want, or we can implement our own time keeping code
 #if USE_LIBC_TIME
@@ -274,9 +365,8 @@ void clock_service_init(clock_service *service)
     // set_zone(0);
     // set daylight savings time to usa_dst function
     set_dst(usa_dst);
-    // set system time default based on localtime 7*ONE_HOUR
-    // if set to 0 (GMT epoch) it will be 6 hours off underflowing to Feb 6 2136 when retrieving the time
-    set_system_time(7 * ONE_HOUR);
+    // Be careful with this function, if set to anythin less than -7*ONE_HOUR, the local time will underflow to the year 2136
+    set_system_time(mktime(&service->_time));
 #endif
     service->is_awake = true;
     service->_clock_prescaler = 0xFFFF;
